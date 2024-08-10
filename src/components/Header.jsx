b670102c587f967e643b3f61821b0364457e423f
Andreas Lordos <andreasglordos@gmail.com>
2024-08-09 21:37:01 -0700

    updated gdrive link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
                 </div>
                 <div className="navbar">
                     <Link to="/portfolio" className="mx-2 hover:text-blue-500">Portfolio</Link>
-                    <Link to="https://docs.google.com/document/d/1krQDQF0vdmlpEbcMIqxd3jkJbStPEOVcz0mQPbAhSEw/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="mx-2 hover:text-blue-500">Resume</Link>
+                    <Link to="https://docs.google.com/viewer?url=https://docs.google.com/document/d/1krQDQF0vdmlpEbcMIqxd3jkJbStPEOVcz0mQPbAhSEw/export?format=pdf" target="_blank" rel="noopener noreferrer" className="mx-2 hover:text-blue-500">Resume</Link>
                     <Link to="/photo" className="mx-2 hover:text-blue-500">Photo</Link>
                 </div>
             </div>
